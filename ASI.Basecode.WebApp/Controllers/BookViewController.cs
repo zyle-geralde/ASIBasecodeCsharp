5aacf6cb44fef57997a438d7a15af7b549e56652
zyle-geralde <zylgerdlp19@gmail.com>
2025-06-13 11:10:00 +0800

    feat(feature): Book list implemented

--- ASI.Basecode.WebApp/Controllers/BookViewController.cs
+++ ASI.Basecode.WebApp/Controllers/BookViewController.cs
@@ -1,11 +1,23 @@
-﻿using Microsoft.AspNetCore.Authorization;
+﻿using ASI.Basecode.Data.Models;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using ASI.Basecode.Services.Interfaces;
 
 namespace ASI.Basecode.WebApp.Controllers
 {
     //This controller will handle actions that return views
     public class BookViewController : Controller
     {
+
+        private readonly IBookService _bookService;
+        public BookViewController(IBookService bookService)
+        {
+            _bookService = bookService;
+        }
+
+
         [HttpGet]
         [Route("Book/AddBook")]
         [AllowAnonymous]// Bypass authorization. No need to Log In 
@@ -13,6 +25,16 @@ namespace ASI.Basecode.WebApp.Controllers
         {
             // This will look for a view at /Views/Books/AddBook.cshtml
             return View("~/Views/Books/AddBook.cshtml");
+        }
+
+        
+        [HttpGet]
+        [Route("Book/ListBook")]
+        [AllowAnonymous]
+        public async Task<IActionResult> ListBook()
+        {
+            List<Book> books = await _bookService.GetAllBooks();
+            return View("~/Views/Books/ListBook.cshtml",books); 
         }
     }
 }
