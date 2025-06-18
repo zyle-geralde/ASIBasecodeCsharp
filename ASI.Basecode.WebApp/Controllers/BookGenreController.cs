b9ff5f58c06082a31553156ed8b62345a12ef0b7
zyle-geralde <zylgerdlp19@gmail.com>
2025-06-19 05:23:16 +0800

    feat(backend): Implemented Add Book Genre Controller

---
+++ ASI.Basecode.WebApp/Controllers/BookGenreController.cs
@@ -0,0 +1,25 @@
+﻿using ASI.Basecode.Services.Interfaces;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ASI.Basecode.WebApp.Controllers
+{
+    public class BookGenreController : Controller
+    {
+        private readonly IBookGenreService BookGenreService;
+
+        public BookGenreController(IBookGenreService book_genre_service)
+        {
+            BookGenreService = book_genre_service;
+        }
+
+        [HttpGet]
+        [Route("BookGenre/AddGenre")]
+        [AllowAnonymous]
+
+        public IActionResult AddGenre()
+        {
+            return View("~Views/BookGenres/AddGenre");
+        }
+    }
+}
