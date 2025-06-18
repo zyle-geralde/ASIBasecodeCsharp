10c62069d6f4b495be35ab8ba19fd03cdaa061ac
Alex Abainza <abainza.alexandra@gmail.com>
2025-06-19 00:15:21 +0800

    feat: implemented view profile page

---
+++ ASI.Basecode.Services/ServiceModels/PersonProfileViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ASI.Basecode.Services.ServiceModels
+{
+    public class PersonProfileViewModel
+    {
+        public string UserId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string AboutMe { get; set; }
+
+    }
+}
