a2a9ac836100b5a1c27eefc0526199a80eea474f
Alex Abainza <abainza.alexandra@gmail.com>
2025-06-19 12:07:30 +0800

    feat: integrated edit person profile page with backend

--- ASI.Basecode.WebApp/Controllers/PersonProfileController.cs
+++ ASI.Basecode.WebApp/Controllers/PersonProfileController.cs
@@ -9,12 +9,10 @@ namespace ASI.Basecode.WebApp.Controllers
     public class PersonProfileController : Controller
     {
         private readonly IPersonProfileService _personProfileService;
-        private readonly IUserService _userService;
 
-        public PersonProfileController(IPersonProfileService profileService, IUserService userService)
+        public PersonProfileController(IPersonProfileService profileService)
         {
             _personProfileService = profileService;
-            _userService = userService;
         }
 
         [HttpGet]
@@ -33,11 +31,58 @@ namespace ASI.Basecode.WebApp.Controllers
             {
                 UserId = profile.ProfileID,
                 FirstName = profile.FirstName,
+                MiddleName = profile.MiddleName,
                 LastName = profile.LastName,
-                AboutMe = profile.AboutMe
+                AboutMe = profile.AboutMe,
+                Birthdate = profile.BirthDate,
+                Gender = profile.Gender,
+                Location = profile.Location,
             };
 
             return View(vm);
         }
+
+        [HttpGet]
+        public async Task<IActionResult> Edit()
+        {
+            var username = User.FindFirstValue(ClaimTypes.NameIdentifier);
+            if (string.IsNullOrEmpty(username))
+            {
+                return Challenge();
+            }
+            var profile = await _personProfileService.GetPersonProfile(username);
+            if (profile == null)
+                return NotFound("Profile not found.");
+
+            var vm = new PersonProfileViewModel
+            {
+                UserId = profile.ProfileID,
+                FirstName = profile.FirstName,
+                MiddleName = profile.MiddleName,
+                LastName = profile.LastName,
+                AboutMe = profile.AboutMe,
+                Birthdate = profile.BirthDate,
+                Gender = profile.Gender,
+                Location = profile.Location,
+            };
+
+            return View(vm);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(PersonProfileViewModel model)
+        {
+            if(!ModelState.IsValid)
+                return View("~/Views/PersonProfile/Edit.cshtml", model);
+
+            var updated = await _personProfileService.EditPersonProfile(model);
+            if (!updated)
+                return NotFound();
+
+            return RedirectToAction("Index");
+
+        }
+
     }
 }
