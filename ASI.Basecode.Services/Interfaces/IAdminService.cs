37e842e83c2368b00e3b7cd2075c1cb398b4ee4e
zyle-geralde <zylgerdlp19@gmail.com>
2025-06-23 16:11:13 +0800

    feat (backend): Added admin register service

---
+++ ASI.Basecode.Services/Interfaces/IAdminService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ASI.Basecode.Services.Interfaces
+{
+    interface IAdminInterface
+    {
+    }
+}
--- ASI.Basecode.Services/ServiceModels/UserViewModel.cs
+++ ASI.Basecode.Services/ServiceModels/UserViewModel.cs
@@ -9,10 +9,10 @@ namespace ASI.Basecode.Services.ServiceModels
 {
     public class UserViewModel
     {
-        [Required(ErrorMessage = "Username is required.")]
+        [Required(ErrorMessage = "Email is required.")]
         public string Email { get; set; }
 
-        [Required(ErrorMessage = "Name is required.")]
+        [Required(ErrorMessage = "UserName is required.")]
         public string UserName { get; set; }
 
         [Required(ErrorMessage = "Password is required.")]
