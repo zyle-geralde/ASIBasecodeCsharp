d8918155934a13235c83a5acee5481fa739967ac
laurenx <santillanmitchlauren@gmail.com>
2025-06-24 12:29:25 +0800

    feat: Authors

---
+++ ASI.Basecode.WebApp/Controllers/AuthorController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System;
+using ASI.Basecode.Services.ServiceModels;
+
+namespace ASI.Basecode.WebApp.Controllers
+{
+    public class AuthorController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
