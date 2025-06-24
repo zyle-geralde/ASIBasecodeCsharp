13cb42fd028827e454dccd694b0506ce49e6595e
zyle-geralde <zylgerdlp19@gmail.com>
2025-06-24 08:14:29 +0800

    feat(migrations): Added migrations - Removed ProfileId on user and admin

---
+++ ASI.Basecode.Data/Migrations/20250624000654_Removed_ProfileId.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ASI.Basecode.Data.Migrations
+{
+    public partial class Removed_ProfileId : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ProfileID",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "ProfileID",
+                table: "Admins");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "ProfileID",
+                table: "Users",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "ProfileID",
+                table: "Admins",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+    }
+}
