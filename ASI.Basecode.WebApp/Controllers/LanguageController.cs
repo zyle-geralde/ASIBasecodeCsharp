b5826dd08b9b5f6552ff56072654794f7e26ab09
zyle-geralde <zylgerdlp19@gmail.com>
2025-06-25 13:24:39 +0800

    feat(backend): Created Repository for Language

--- ASI.Basecode.WebApp/Controllers/LanguageController.cs
+++ ASI.Basecode.WebApp/Controllers/LanguageController.cs
@@ -1,9 +1,14 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 
 namespace ASI.Basecode.WebApp.Controllers
 {
     public class LanguageController : Controller
     {
+
+        [HttpGet]
+        [Route("Language/Index")]
+        [AllowAnonymous]
         public IActionResult Index()
         {
             return View();
