7aaaf27d6549f794a8bb9f65d8b385f4dd295839
Alex Abainza <abainza.alexandra@gmail.com>
2025-06-25 17:38:30 +0800

    chore: added top 3 books to top rated and newly added books. also linked the books to the book details page

--- ASI.Basecode.Services/Services/BookService.cs
+++ ASI.Basecode.Services/Services/BookService.cs
@@ -15,7 +15,6 @@ namespace ASI.Basecode.Services.Services
     public class BookService : IBookService
     {
         private readonly IBookRepository _bookRepository;
-
         public BookService(IBookRepository bookRepository)
         {
             _bookRepository = bookRepository;
@@ -89,8 +88,12 @@ namespace ASI.Basecode.Services.Services
                 Author = b.Author,
                 AverageRating = b.AverageRating,
                 CoverImage =b.CoverImage,
-                BookFile=b.BookFile
+                BookFile=b.BookFile,
+                Description = b.Description
+
             }).ToList();
+
+
         }
         public async Task<BookViewModel?> GetBookById(string bookId)
         {
