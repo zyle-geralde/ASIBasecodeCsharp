609d497eca6a2cad12a5c80764e0c0ca7e189056
Khaleng97 <kharylleann.iglesias8@gmail.com>
2025-07-03 14:26:28 +0800

    Merged with remaining-tasks branch

---
+++ ASI.Basecode.Services/ServiceModels/ChangePasswordViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ASI.Basecode.Services.ServiceModels
+{
+    public class ChangePasswordViewModel
+    {
+        public int UserId { get; set; }
+
+        [Required, DataType(DataType.Password)]
+        public string CurrentPassword { get; set; }
+
+        [Required, DataType(DataType.Password)]
+        public string NewPassword { get; set; }
+
+        [Required, Compare("NewPassword"), DataType(DataType.Password)]
+        public string ConfirmPassword { get; set; }
+    }
+}
