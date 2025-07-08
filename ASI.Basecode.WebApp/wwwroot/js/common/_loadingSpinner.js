b3a23f9c4a3b8f0fc128549a9cffe85428be8423
laurenx <santillanmitchlauren@gmail.com>
2025-07-09 02:44:57 +0800

    feat: loadingSpinner when delete and for consistency
    + made a loadingSpinner js and implemented it in the list book and author list
    + fixed the design of the author list (action buttons)

---
+++ ASI.Basecode.WebApp/wwwroot/js/common/_loadingSpinner.js
@@ -0,0 +1,7 @@
+﻿export function showLoadingOverlay() {
+    document.getElementById('loadingOverlay').style.display = 'flex';
+}
+
+export function hideLoadingOverlay() {
+    document.getElementById('loadingOverlay').style.display = 'none';
+}
